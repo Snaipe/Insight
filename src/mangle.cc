72244e1afe681b6ce581371f7a0a4b5c1de1f7b8
Snaipe <franklinmathieu@gmail.com>
2015-06-03 04:31:32 +0200

    Added GCC demangling utility

---
+++ src/mangle.cc
@@ -0,0 +1,17 @@
+#include "mangle.h"
+
+#ifdef __GNUG__
+#include <cxxabi.h>
+#include <memory>
+
+std::string Insight::demangle(std::string&& name) {
+    int status;
+    std::unique_ptr<char, void(*)(void*)> res(abi::__cxa_demangle(name.c_str(), 0, 0, &status), std::free);
+    return status ? name : res.get();
+};
+
+#else
+std::string Insight::demangle(std::string&& name) {
+    return name;
+}
+#endif
---
+++ src/mangle.h
@@ -0,0 +1,10 @@
+#ifndef INSIGHT_MANGLE_H
+# define INSIGHT_MANGLE_H
+
+# include <string>
+
+namespace Insight {
+    std::string demangle(std::string &&name);
+}
+
+#endif /* !INSIGHT_MANGLE_H */
