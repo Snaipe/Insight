332f6f29e11dbd450a0bd43aeaf50cbd5c3a7e6b
Snaipe <franklinmathieu@gmail.com>
2015-08-28 04:39:58 +0200

    Added tests for non-virtual class methods

--- tests/class.cc
+++ tests/class.cc
@@ -7,6 +7,7 @@ class ClassTest {
 public:
     ClassTest();
     int get_bar() const;
+    void set_bar(int bar);
 private:
     int bar;
 };
@@ -15,6 +16,10 @@ ClassTest::ClassTest() : bar(0) {}
 
 int ClassTest::get_bar() const {
     return bar;
+}
+
+void ClassTest::set_bar(int bar) {
+    this->bar = bar;
 }
 
 TEST(Class, Field) {
@@ -30,5 +35,19 @@ TEST(Class, Field) {
     EXPECT_EQ(42, instance.get_bar());
 
     field.set(instance, 24);
+    EXPECT_EQ(24, instance.get_bar());
+}
+
+TEST(Class, Method) {
+    ClassTest instance;
+
+    auto& type = type_of(instance);
+    auto& get_bar = type.method("get_bar");
+    auto& set_bar = type.method("set_bar");
+
+    instance.set_bar(42);
+    EXPECT_EQ(42, get_bar.call<int>(instance));
+
+    set_bar.call<void>(instance, 24);
     EXPECT_EQ(24, instance.get_bar());
 }
